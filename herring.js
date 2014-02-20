94cca9d11567a2e4584c84364df407ad2221d64d
Thomas Kerber <t.kerber@online.de>
2014-02-20 15:36:27 +0000

    Split data.js

--- herring.js
+++ herring.js
@@ -128,6 +128,7 @@ function initialize() {
   button(" Primary ", showingPrimarySchools);
   button(" Secondary ", showingSecondarySchools);
 
+    console.log("hey");
   for(var key in schools){
     schools[key].draw();
     for(var i = 0; i < schools[key].conns.length; i++){
@@ -139,9 +140,6 @@ function initialize() {
   redraw(); //draw all schools
   drawZones("Education");
 }
-
-//run the initialise function on load
-google.maps.event.addDomListener(window, 'load', initialize);
 
   
 function searchBar() {
